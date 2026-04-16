c46043bc0c1ad548e32905c3ac948f13ecc78088
EomTeaEun <xodms3320@gmail.com>
2026-04-16 16:00:44 +0900

    codes update

--- code/3-3.c
+++ code/3-3.c
@@ -3,11 +3,18 @@
 int main() {
     int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
-    for (int i = 0; i < 2; i++) { 
-        for (int j = 0; j < 3; j++) { 
-            printf("%p\n", (void*)&arr[i][j]); 
-        }
-    }
+    int (*p1)[2][3] = &arr;
+    int (*p2)[3] = &arr[0];
+    int *p3 = &arr[0][0];
+
+    printf("%p\n", (void*)p1);
+    printf("%p\n", (void*)p2);
+    printf("%p\n", (void*)p3);
     printf("\n");
+
+    printf("%lu\n", (unsigned long)((char*)(p1 + 1) - (char*)p1)); 
+    printf("%lu\n", (unsigned long)((char*)(p2 + 1) - (char*)p2)); 
+    printf("%lu\n", (unsigned long)((char*)(p3 + 1) - (char*)p3)); 
+
     return 0;
 }
